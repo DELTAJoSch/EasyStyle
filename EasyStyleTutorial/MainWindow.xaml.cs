2460b44a745f49155cf822d6f7a54488870ad9c7
DELTAJoSch <deltajosch@gmail.com>
2020-04-10 20:49:27 +0200

    added tutorial project

---
+++ EasyStyleTutorial/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using EasyStyle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyStyleTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : WindowBase
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("My first message!");
+        }
+    }
+}
